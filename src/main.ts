c7e47fbb15260b134fb30cf2b3db0e1eeb609154
Nurlord <352933nurzat@gmail.com>
2025-06-12 23:02:03 +0500

    add serve static tree

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import appRootPath from 'app-root-path';
+import { join } from 'path';
+import serveIndex from 'serve-index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +13,10 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-
+  const uploadsPath = join(appRootPath.path, 'uploads');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
+  app.use('/uploads', express.static(uploadsPath));
+  app.use('/uploads', serveIndex(uploadsPath, { icons: true }));
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ApiDocs')
     .addBearerAuth()
